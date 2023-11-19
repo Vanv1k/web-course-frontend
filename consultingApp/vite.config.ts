b2edf17a761944c18f28138d7e7fa474282981f1
Vanv1k <ivanspecial5@gmail.com>
2023-11-19 20:13:51 +0300

    ltfi

--- consultingApp/vite.config.ts
+++ consultingApp/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Vanv1k/web-course-frontend/tree/React_SPA',
+  base: '/web-course-frontend/',
   plugins: [react()],
   server: {
     proxy: {
