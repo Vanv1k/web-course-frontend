5037ed54cf7f51bdf953df7d09ac9ad0d9e634bf
Vanv1k <ivanspecial5@gmail.com>
2023-11-19 20:37:33 +0300

    basename

--- consultingApp/src/App.tsx
+++ consultingApp/src/App.tsx
@@ -4,7 +4,7 @@ import ProductPage from './pages/ProductPage/ProductPage.tsx';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={"/web-course-frontend/"}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/consultations/:id" element={<ProductPage />} />
