848c674ba363e5eea052966a7c39229474c6a054
Vanv1k <ivanspecial5@gmail.com>
2023-11-19 21:33:50 +0300

    final

--- consultingApp/vite.config.ts
+++ consultingApp/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/web-course-frontend/',
   plugins: [react()],
   server: {
     proxy: {
