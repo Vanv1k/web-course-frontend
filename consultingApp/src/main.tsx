84a7d17e4652e0b3778047ddd5bb53f7b4f2cdcd
Vanv1k <ivanspecial5@gmail.com>
2023-11-19 20:29:30 +0300

    return

--- consultingApp/src/main.tsx
+++ consultingApp/src/main.tsx
@@ -1,14 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-    </BrowserRouter>
-</React.StrictMode>,
 )
